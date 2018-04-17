3a921d9bbeba5dfc3b47748eb9bb5ec5ab4360ab
AIndoria <indoriaabhishek@gmail.com>
2018-04-16 23:56:36 -0700

    add reduce instead of while loops.

--- diffTwoArrays.js
+++ diffTwoArrays.js
@@ -5,31 +5,17 @@ Take each array. Compare each element with every element of the other array, pus
 
 */
 
-arr1=[1,2,3,4,5];
-arr2=[2,4,6,8];
-finArray=[];
+//This works better than my last solution. It's still bad code but hey, it works.
 
-for(let i=0; i<arr1.length;i++){
-  for(let j=0; j<arr2.length;j++){
-    var check1=arr1.indexOf(arr2[j]);
-    var check2=arr2.indexOf(arr1[i]);
-    while(check1==-1){
-      while(finArray.indexOf(arr2[j])==-1){
-        finArray.push(arr2[j]);
-        break;
-      }
-      break;
-    }
-  }
-  while(check2==-1){
-    while(finArray.indexOf(arr1[i])==-1){
-      finArray.push(arr1[i]);
-      break;
-    }
-    break;
-  }
+
+function diffArray(...arrs){
+  let filteredArray1=arrs[1].filter(function(element){
+    return !arrs[0].includes(element);
+  });
+  let filteredArray2=arrs[0].filter(function(element){
+    return !arrs[1].includes(element)
+  });
+  return filteredArray1.concat(filteredArray2);
 }
-finArray.sort((a,b)=>a-b);
-console.log(finArray);
 
-//This apparently doesn't work for empty arrays. Also, too many "while" statements.
+diffArray([1,4,5,8], [1,2,3]);
